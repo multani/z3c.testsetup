0578f060d01567cd7caae25a7ada35774dfd894e
ulif <ulif@c3247942-daef-42da-abc4-cf9e8ebdb6ce>
2008-02-01 00:14:46 +0000

    Add setup.py.
    git-svn-id: http://svn.zope.org/repos/main/Sandbox/ulif/z3c-testsetup/trunk@83360 c3247942-daef-42da-abc4-cf9e8ebdb6ce

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n\n'
+    + read('CHANGES.txt')
+    + '\n\n'
+    )
+
+setup(
+    name='z3c.testsetup',
+    version='0.1',
+    author='Uli Fouquet',
+    author_email='uli@gnufix.de',
+    url='',
+    download_url='',
+    description='Easier test setup for Grok and Zope 3 components.',
+    long_description=long_description,
+    license='ZPL',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Environment :: Web Environment',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: Zope Public License',
+                 'Programming Language :: Python',
+                 'Framework :: Zope3',
+                 ],
+
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages = ['z3c'],
+    include_package_data = True,
+    zip_safe=False,
+    install_requires=['setuptools',
+                      'zope.testing',
+                      'zope.app.testing',
+                      ],
+)
