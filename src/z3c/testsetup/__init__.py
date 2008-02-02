3b6a7383ad2a3f2944a81346c34599b2d04883e6
ulif <ulif@c3247942-daef-42da-abc4-cf9e8ebdb6ce>
2008-02-02 15:52:49 +0000

    Move testing to doctesting.
    git-svn-id: http://svn.zope.org/repos/main/Sandbox/ulif/z3c-testsetup/trunk@83427 c3247942-daef-42da-abc4-cf9e8ebdb6ce

--- src/z3c/testsetup/__init__.py
+++ src/z3c/testsetup/__init__.py
@@ -1,3 +1,3 @@
-from z3c.testsetup.testing import (UnitDocTestSetup, FunctionalDocTestSetup,
-                                   register_doctests)
+from z3c.testsetup.doctesting import (UnitDocTestSetup, FunctionalDocTestSetup,
+                                      register_doctests)
 from z3c.testsetup.base import BasicTestSetup
