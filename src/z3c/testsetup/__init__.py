9888cf97cd4fb0544ce5c3bb0913acf5878fdd5d
ulif <ulif@c3247942-daef-42da-abc4-cf9e8ebdb6ce>
2008-02-05 07:04:57 +0000

    Update module init.
    git-svn-id: http://svn.zope.org/repos/main/Sandbox/ulif/z3c-testsetup/trunk@83521 c3247942-daef-42da-abc4-cf9e8ebdb6ce

--- src/z3c/testsetup/__init__.py
+++ src/z3c/testsetup/__init__.py
@@ -1,8 +1,8 @@
 from z3c.testsetup.base import BasicTestSetup
 from z3c.testsetup.doctesting import (UnitDocTestSetup, FunctionalDocTestSetup,
-                                      register_doctests, collect_doctests)
+                                      register_doctests, get_doctests_suite)
 from z3c.testsetup.testing import (UnitTestSetup, register_pytests,
-                                   collect_pytests)
+                                   get_pytests_suite)
 from z3c.testsetup.util import get_package
 import unittest
 
@@ -10,7 +10,7 @@ def register_all_tests(pkg_or_dotted_name, *args, **kwargs):
     pkg = get_package(pkg_or_dotted_name)
     def tempfunc():
         suite = unittest.TestSuite()
-        suite.addTest(collect_pytests(pkg, *args, **kwargs))
-        suite.addTest(collect_doctests(pkg, *args, **kwargs))
+        suite.addTest(get_pytests_suite(pkg, *args, **kwargs))
+        suite.addTest(get_doctests_suite(pkg, *args, **kwargs))
         return suite
     return tempfunc
