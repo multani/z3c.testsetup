7177f27f75de2ca14657309fac0ece70324f3899
ulif <ulif@c3247942-daef-42da-abc4-cf9e8ebdb6ce>
2008-02-14 13:15:32 +0000

    Add checker tests.
    git-svn-id: http://svn.zope.org/repos/main/Sandbox/ulif/z3c-testsetup/trunk@83830 c3247942-daef-42da-abc4-cf9e8ebdb6ce

---
+++ src/z3c/testsetup/tests/cave/samplesetup_short5.py
@@ -0,0 +1,14 @@
+import re
+from zope.testing import renormalizing
+import z3c.testsetup
+mychecker = renormalizing.RENormalizing([
+    (re.compile('[0-9]*[.][0-9]* seconds'), 
+     '<SOME NUMBER OF> seconds'),
+    (re.compile('at 0x[0-9a-f]+'), 'at <SOME ADDRESS>'),
+    ])
+test_suite = z3c.testsetup.register_all_tests(
+    'z3c.testsetup.tests.cave',
+    checker = mychecker,
+    extensions = ['.chk',],
+    fregexp_list = ['.*checker.*',],
+    )
---
+++ src/z3c/testsetup/tests/cave/samplesetup_short6.py
@@ -0,0 +1,8 @@
+import re
+from zope.testing import renormalizing
+import z3c.testsetup
+test_suite = z3c.testsetup.register_all_tests(
+    'z3c.testsetup.tests.cave',
+    extensions = ['.chk',],
+    fregexp_list = ['.*checker.*',],
+    )
