d97bc71ad08b05e3d70b2af762a6ed39761ef79a
ulif <ulif@c3247942-daef-42da-abc4-cf9e8ebdb6ce>
2008-07-28 14:46:55 +0000

    Add a ZCML layer definition.
    git-svn-id: http://svn.zope.org/repos/main/z3c.testsetup/trunk@88867 c3247942-daef-42da-abc4-cf9e8ebdb6ce

--- src/z3c/testsetup/tests/layered_cave/layer.py
+++ src/z3c/testsetup/tests/layered_cave/layer.py
@@ -1,5 +1,29 @@
+"""Layer definitions.
 
-layer_marker = None
+This could also be done in the setup file itself.
+"""
+
+import os
+from zope.app.testing.functional import ZCMLLayer
+
+# We define a ZCML test layer. ZCML layers are special as they define
+# some setup code for creation of empty ZODBs and more. If you only
+# want some ZCML registrations to be done, you can use it like so:
+FunctionalLayer1 = ZCMLLayer(
+    # As first argument we need the absolute path of a ZCML file
+    os.path.join(os.path.dirname(__file__), 'ftesting.zcml'),
+
+    # Second argument is the module, where the layer is defined.
+    __name__,
+
+    # This is the name of our layer. It can be an arbitrary string.
+    'FunctionalLayer1',
+
+    # By default ZCML layers are not torn down. You should make sure,
+    # that any registrations you do in your ZCML are removed in a
+    # tearDown method if you specify this parameter to be `True`. This
+    # parameter is optional.
+    allow_teardown = True)
 
 class UnitLayer1(object):
     """This represents a layer.
