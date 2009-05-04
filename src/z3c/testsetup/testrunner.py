ce3c6a442b72a60f7979b4b28ffdcd4fa4368983
ulif <ulif@c3247942-daef-42da-abc4-cf9e8ebdb6ce>
2009-05-04 10:03:50 +0000

    Add convenience wrappers for zope.testing.testrunner.
    git-svn-id: http://svn.zope.org/repos/main/z3c.testsetup/trunk@99697 c3247942-daef-42da-abc4-cf9e8ebdb6ce

---
+++ src/z3c/testsetup/testrunner.py
@@ -0,0 +1,24 @@
+##############################################################################
+#
+# Copyright (c) 2009 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Testrunner convenience stuff.
+"""
+from zope.testing import testrunner
+from zope.testing.testrunner import run
+
+# Convenience mapping to have run_internal() and run() always
+# available and refering to the same function from zope.testing. See
+# `testrunner.txt` (bottom) for details.
+if hasattr(testrunner, 'run_internal'):
+    run = testrunner.run_internal
+run_internal = run
