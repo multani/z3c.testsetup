36b65f5ae85749628bc46cdd505b400627347ce5
janwijbrand <janwijbrand@c3247942-daef-42da-abc4-cf9e8ebdb6ce>
2010-05-17 15:55:39 +0000

    Preparing release 0.7
    git-svn-id: http://svn.zope.org/repos/main/z3c.testsetup/trunk@112417 c3247942-daef-42da-abc4-cf9e8ebdb6ce

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ long_description = (
 
 setup(
     name='z3c.testsetup',
-    version = '0.7dev',
+    version='0.7',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     url = 'http://pypi.python.org/pypi/z3c.testsetup',
